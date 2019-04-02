e39de5541d08559105c544572b949862172761e9
electricSoda <justin.hua.ge@gmail.com>
2019-04-01 20:55:07 -0600

    Make format better

--- credits.py
+++ credits.py
@@ -4,6 +4,7 @@
 #NOTE: (delete after reading) go here for the main window: https://www.youtube.com/watch?v=C-t0nZ_8HeE
 #import modules and scripts needed
 import tkinter
+from time import *
 from tkinter import *
 
 
@@ -11,45 +12,43 @@ from tkinter import *
 #getOUT
 def getOUT():
       root1.destroy()
-      
 
+names = ['Justin Ge', 'Brian Wiggins', 'Max Vandershaf', 'and Python (the programming language)', ' ']
 
 #GUI('s)
 def guiBack():
-      global root1
+    global root1
 
-      root1 = Tk()
+    root1 = Tk()
 
-      root1.title('Multi-Snake Credits')
+    root1.title('Multi-Snake Credits')
 
-      #set the color
-      root1.config(bg='black')
+    #set the color
+    root1.config(bg='black')
 
-      root1.attributes('-fullscreen', True)
+    root1.attributes('-fullscreen', True)
 
-      cred = Label(root1, text='Credits:')
-      cred.config(fg='white', bg='black')
-      cred.config(font=('Helvetica', 40))
-      cred.pack()
+    cred = Label(root1, text='Credits:')
+    cred.config(fg='white', bg='black')
+    cred.config(font=('Helvetica', 40))
+    cred.pack()
 
-      name = Label(root1, text='Justin Ge')
-      name.config(fg='white', bg='black')
-      name.config(font=('Helvetica', 35))
-      name.pack()
+    name = Label(root1)
+    name.config(fg='white', bg='black')
+    name.config(font=('Helvetica', 35))
+    name.pack()
 
-      name1 = Label(root1, text='Brian Wiggins')
-      name1.config(fg='white', bg='black')
-      name1.config(font=('Helvetica', 35))
-      name1.pack()
+    for i in range(len(names)):
+        name['text']=names[i]
+        root1.update()
+        sleep(4)
 
-      name2 = Label(root1, text='Max Vandershaf')
-      name2.config(fg='white', bg='black')
-      name2.config(font=('Helvetica', 35))
-      name2.pack()
+    cred.pack_forget()
 
-      exi = Button(root1, text='Back', command=getOUT)
-      exi.config(fg='black', bg='white')
-      exi.pack(side=BOTTOM)
+    exi = Button(root1, text='Back', command=getOUT)
+    exi.config(fg='black', bg='white')
+    exi.config(font=('Helvetica', 20))
+    exi.pack(side=BOTTOM)
 
 
-      root1.mainloop()
+    root1.mainloop()
